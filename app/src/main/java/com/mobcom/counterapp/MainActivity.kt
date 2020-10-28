3f1d8d7321381a84da9fe82a57549495cf5f75e3
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-10-28 15:05:03 +0700

    Home Fragment

--- app/src/main/java/com/mobcom/counterapp/MainActivity.kt
+++ app/src/main/java/com/mobcom/counterapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.mobcom.counterapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+    private var counter : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv_counter = findViewById<TextView>(R.id.tv_counter)
+        val add_button = findViewById<ImageButton>(R.id.add)
+        val subs_button = findViewById<ImageButton>(R.id.subs)
+
+        tv_counter.setText("" + counter)
+
+        add_button.setOnClickListener() {
+            counter += 1
+            tv_counter.setText("" + counter)
+        }
+
+        subs_button.setOnClickListener() {
+            counter -= 1
+            tv_counter.setText("" + counter)
+        }
     }
 }
